e272561de8ef70a5f18443148d8a313854a631b4
VanCuong <tranvancuong99987@gmail.com>
2026-05-21 12:54:24 +0700

    save

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,7 +11,7 @@ export default function AppRoutes() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
 
-        { <Route path="/register" element={<RegisterPage />} /> }
+        { <Route path="/register" element={<RegisterPage />} />  }
 
         <Route
           path="/forgot-password"
